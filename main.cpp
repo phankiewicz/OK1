d74eccf1306a2571323f814df6c55ead36fd3b65
Radmor <p.hankiewicz@gmail.com>
2015-11-22 12:46:29 +0100

    Dodanie wczytywania danych dla liczby klientow podanej jako ostatni parametr wywolania

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <fstream>
 #include <vector>
+#include <list>
+#include <cmath>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -18,7 +21,7 @@ public:
 
 //Zmienne globalne
 int vehiclesNumber;
-int capacity;
+int vehiclesCapacity;
 
 vector<Customer> customersVector;
 
@@ -31,7 +34,7 @@ void print_customersVector(vector<Customer>& v) {
 }
 
 
-//Funkcja wczytujaca dane z pliku
+//Funkcja wczytujaca dane z pliku w formacie solomona
 void data_input(char * filename){
 
     string truncLine;
@@ -44,10 +47,10 @@ void data_input(char * filename){
     for(int i=0;i<4;i++)
         getline(file,truncLine);
 
-    file>>vehiclesNumber>>capacity;
+    file>>vehiclesNumber>>vehiclesCapacity;
 
     //Wlaczenie wypisywania kontrolnego liczby ciezarowek i ich ladownosci
-    //printf("%d %d\n", vehiclesNumber,capacity);
+    //printf("%d %d\n", vehiclesNumber,vehiclesCapacity);
 
     //Omijanie kolejnych linii bez danych
     for(int i=0;i<4;i++)
@@ -68,13 +71,105 @@ void data_input(char * filename){
     file.close();
 }
 
-int main(int args, char* argv[]) {
+//Funkcja wczytywania okreslonej liczby danych
+void data_input_n(char * filename, char * data_quantity){
+    string truncLine;
+    fstream file;
+    file.open(filename, fstream::in);
+    int n;
+
+    Customer temp;
+
+    //Omijanie wstepnych linii bez danych
+    for(int i=0;i<4;i++)
+        getline(file,truncLine);
+
+    file>>vehiclesNumber>>vehiclesCapacity;
+
+    //Wlaczenie wypisywania kontrolnego liczby ciezarowek i ich ladownosci
+    //printf("%d %d\n", vehiclesNumber,vehiclesCapacity);
+
+    //Omijanie kolejnych linii bez danych
+    for(int i=0;i<4;i++)
+        getline(file,truncLine);
+
+    n=atoi(data_quantity);
+
+    //Wczytywanie danych magazynu i n klientow
+
+    for(int i=0;i<=n && !file.eof();i++)
+    {
+        file>>temp.id>>temp.x>>temp.y>>temp.demand>>temp.ready_time>>temp.due_date>>temp.service_duration;
+        customersVector.push_back(temp);
+    }
 
 
-    //Wczytywanie danych
-    data_input(argv[1]);
+    //Wyrzucanie ostatniego elementu z vectora, gdy dotrzemy do konca pliku (ostatni element jest taki sam jak przedostatni)
+    if(file.eof()){
+        customersVector.pop_back();
+    }
+
+    //Wlaczanie kontrolnego wypisywania vectora
+    //print_customersVector(customersVector);
+
+    file.close();
+}
+
+/*
+
+//Funkcja obliczajaca dlugosc trasy miedzy dwoma punktami
+double distance(Customer &point1, Customer &point2){
+
+    return sqrt(((point1.x-point2.x)*(point1.x-point2.x))+((point1.y-point2.y)*(point1.y-point2.y)));
+
+}
 
 
-    
+
+//Funkcja sprawdzajaca poprawnosc trasy
+int computeRoute(list<Customer> routeNodesList) {
+    list<Customer>:: iterator it1=routeNodesList.begin();
+    list<Customer>::iterator it2=routeNodesList.begin();
+    it2++;
+    int distanceSum=0;
+    int capacitySum=0;
+
+    for(it2; it2!=routeNodesList.end();++it2) {
+        if(distanceSum+distance(it1,it2)<=routeNodesList[it2].due_time){
+            distanceSum+=distance(it1,it2);
+            if(distanceSum<it2.ready_time){
+                distanceSum=it2.ready_time;
+            }
+            distanceSum+=it2.service_duration;
+
+        }
+        else
+            return -1;
+        if(capacitySum+it2.demand<=vehiclesCapacity)
+            capacitySum+=it2.demand;
+        else
+            return -1;
+
+        it1++;
+    }
+    return distanceSum;
+}
+*/
+
+int main(int argc, char* argv[]) {
+
+    if(argc==2){
+        //Wczytywanie danych
+        data_input(argv[1]);
+
+    }
+        else if(argc==3){
+        //Wczytywanie okreslonej ilosci danych
+        data_input_n(argv[1],argv[2]);
+    }
+    else{
+        printf("Niewlasciwa liczba parametrow\n");
+    }
+
     return 0;
 }
